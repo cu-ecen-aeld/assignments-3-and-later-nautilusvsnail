e3dd2ccba4e3cead19ed7714a3f19015274f0f51
sully <sullymadewell@gmail.com>
2024-08-16 18:18:14 -0400

    initial C writer implementation

---
+++ finder-app/writer.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <syslog.h>
+
+
+int main(int argc, char *argv[]) {
+    
+    openlog(NULL,0,LOG_USER);
+    if (argc != 2) {
+        syslog(LOG_ERR,"Invalid Number of Arguments: %d",argc);
+        return 1;
+    }
+
+    char *write_file = argv[0];
+    char *write_string = argv[1];
+
+    FILE *file = fopen(write_file, "w");
+    if (file == NULL) {
+        syslog(LOG_ERR, "Failed to open file for writing: %s", write_file);
+        return 1;
+    }
+
+    fprintf(file, write_string);
+    fclose(file);
+
+    syslog(LOG_INFO, "File Written Successfully");
+    closelog();
+
+    return 0;
+}
